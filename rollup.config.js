30f3e6db8f6f14fa33f5a932ce46a6f15f1b3623
jaden.wang <jaden_wong@icloud.com>
2019-11-05 11:23:14 +0800

    chore(tsconfig.json/package.json/rollup.config.js): 构建输出sourceMap和declare文件

--- rollup.config.js
+++ rollup.config.js
@@ -16,12 +16,12 @@ export default {
     // 表示输出文件的内容，它允许传入一个对象或一个数组
     output: [
         // main:定义了npm包的入口文件，browser环境和node环境均可使用
-        { file: pkg.main, name: camelCase(libraryName), format: 'cjs', sourcemap: false },
+        { file: pkg.main, name: camelCase(libraryName), format: 'cjs', sourcemap: true },
         // module:定义npm包的ESM规范的入口文件，browser环境和node环境均可使用
-        { file: pkg.module, name: camelCase(libraryName), format: 'es', sourcemap: false },
+        { file: pkg.module, name: camelCase(libraryName), format: 'es', sourcemap: true },
         // browser: browser字段提供一个文件路径作为在浏览器端使用时的模块入口
         // refs: https://zhuanlan.zhihu.com/p/31499310
-        { file: pkg.browser, name: camelCase(libraryName), format: 'umd', sourcemap: false },
+        { file: pkg.browser, name: camelCase(libraryName), format: 'umd', sourcemap: true },
     ],
     watch: {
         include: 'src/**',
