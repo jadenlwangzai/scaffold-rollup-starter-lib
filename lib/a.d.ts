7fe8324c22d7865562ac3f021d97a267e9f55ac9
jaden.wang <jaden.wang@oyohotels.cn>
2019-11-20 14:52:12 +0800

    feat(构建打包):

--- lib/a.d.ts
+++
@@ -1,3 +0,0 @@
-declare const a = 1;
-export default a;
-//# sourceMappingURL=a.d.ts.map
--- lib/b.d.ts
+++
@@ -1,7 +0,0 @@
-declare class Greeter {
-    greeting: string;
-    constructor(message: string);
-    greet(): string;
-}
-export default Greeter;
-//# sourceMappingURL=b.d.ts.map
